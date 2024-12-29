84afdbcc68627d7ad94868f659d6423e801c0b19
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-29 11:43:10 +0100

    feat: add Container component for layout management

---
+++ components/global/Container.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+
+/**CONTAINER COMPONENT */
+function Container({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+      {children}
+    </div>
+  );
+}
+
+export default Container;
