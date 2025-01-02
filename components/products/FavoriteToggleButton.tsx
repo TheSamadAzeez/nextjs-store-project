d6a4c0ae7d801cda885f209453883585a7109c9f
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-02 14:42:11 +0100

    feat: implement FavoriteToggleButton component with heart icon for favoriting products

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,5 +1,12 @@
-function FavoriteToggleButton() {
-  return <div>FavoriteToggleButton</div>;
+import { Button } from '../ui/button';
+import { FaHeart } from 'react-icons/fa';
+
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+      <FaHeart />
+    </Button>
+  );
 }
 
 export default FavoriteToggleButton;
