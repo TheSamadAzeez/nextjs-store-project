cbd030678dca76898ceb2c5bd346259a4d1c4c16
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-02 21:47:41 +0100

    feat: update LoadingContainer styles for improved layout and padding

--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -14,8 +14,8 @@ function LoadingContainer() {
 function LoadingProduct() {
   return (
     <Card>
-      <CardContent>
-        <Skeleton className='h-48 w-full mt-4' />
+      <CardContent className='p-4'>
+        <Skeleton className='h-48 w-full' />
         <Skeleton className='h-4 w-3/4 mt-4' />
         <Skeleton className='h-4 w-1/2 mt-4' />
       </CardContent>
