867cf677f36eb57f531f0a4832ebc99b0510fa0b
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-03 07:41:41 +0100

    feat: enhance ProductsContainer with layout and search props, and import necessary components

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -1,4 +1,18 @@
-function ProductsContainer() {
+import ProductsGrid from './ProductsGrid';
+import ProductsList from './ProductsList';
+import { LuLayoutGrid, LuList } from 'react-icons/lu';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { fetchAllProducts } from '@/utils/actions';
+import Link from 'next/link';
+
+function ProductsContainer({
+  layout,
+  search,
+}: {
+  layout: string;
+  search: string;
+}) {
   return <div>ProductsContainer</div>;
 }
 
