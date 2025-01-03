0c1678d894255927f9282999852deaba7931565a
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-03 12:12:07 +0100

    feat: implement asynchronous product fetching and conditional rendering in ProductsContainer

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -6,14 +6,34 @@ import { Separator } from '@/components/ui/separator';
 import { fetchAllProducts } from '@/utils/actions';
 import Link from 'next/link';
 
-function ProductsContainer({
+async function ProductsContainer({
   layout,
   search,
 }: {
   layout: string;
   search: string;
 }) {
-  return <div>ProductsContainer</div>;
+  const products = await fetchAllProducts();
+  const totalProducts = products.length;
+  const searchTerm = search ? `&search=${search}` : '';
+  return (
+    <>
+      {/* HEADER */}
+      <section></section>
+      {/* PRODUCTS */}
+      <div>
+        {totalProducts === 0 ? (
+          <h5 className='text-2xl mt-16'>
+            Sorry, no products matched your search criteria ...
+          </h5>
+        ) : layout === 'grid' ? (
+          <ProductsGrid products={products} />
+        ) : (
+          <ProductsList products={products} />
+        )}
+      </div>
+    </>
+  );
 }
 
 export default ProductsContainer;
