e25bf2bf77cc8645ac286d9b325cea6381821d0f
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-05 14:25:19 +0100

    feat: simplify SingleProductPage component by removing searchParams and refining type inference

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,25 +6,12 @@ import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import ProductRating from '@/components/single-product/ProductRating';
 
-// Define params interface separately for better type inference
-interface SingleProductPageParams {
-  params: {
-    id: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
 // Single Product Page Component
 // Displays detailed information about a specific product
-export default async function SingleProductPage({
-  params,
-  searchParams,
-}: SingleProductPageParams) {
+async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
-
-  console.log(searchParams);
 
   return (
     <section>
@@ -57,3 +44,5 @@ export default async function SingleProductPage({
     </section>
   );
 }
+
+export default SingleProductPage;
