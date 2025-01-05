773d6cb5f20e75ce802649a1cdba7f598a140421
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-05 08:44:41 +0100

    feat: enhance single product components with AddToCart, BreadCrumbs, and ProductRating functionality

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,4 +1,10 @@
-function AddToCart() {
-  return <div>AddToCart</div>;
+import { Button } from '../ui/button';
+
+function AddToCart({ productId }: { productId: string }) {
+  return (
+    <Button className='capitalize mt-8' size='lg'>
+      add to cart
+    </Button>
+  );
 }
 export default AddToCart;
--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,4 +1,19 @@
-function ProductRating() {
-  return <div>ProductRating</div>;
+import { FaStar } from 'react-icons/fa';
+
+function ProductRating({ productId }: { productId: string }) {
+  // hardcoded rating and count for now
+  const rating = 4.2;
+  const count = 25;
+
+  const className = `flex gap-1 items-center text-md mt-1 mb-4`;
+  const countValue = `(${count})`;
+
+  return (
+    <span className={className}>
+      <FaStar className='w-3 h-3' />
+      {rating}
+      {countValue}
+    </span>
+  );
 }
 export default ProductRating;
