9847c57f5b4da6e36679fcce81e655660abb4d00
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-09 00:22:27 +0100

    feat: enhance SignOutLink to display a toast notification on successful logout

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,22 @@
+'use client';
+
+import { useToast } from '@/hooks/use-toast';
+import { SignOutButton } from '@clerk/nextjs';
+import Link from 'next/link';
+
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+
+  const handleSignOut = async () => {
+    toast({ description: 'Logout successful' });
+  };
+  return (
+    <SignOutButton>
+      <Link href='/' className='w-full text-left' onClick={handleSignOut}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
