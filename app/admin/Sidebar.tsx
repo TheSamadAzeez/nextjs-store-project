0cc514fd2887124185dc3e1ee983f0f7e5ac88b3
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-11 11:57:05 +0100

    feat: implement Sidebar component for admin navigation

---
+++ app/admin/Sidebar.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { adminLinks } from '@/utils/links';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
+
+function Sidebar() {
+  const pathname = usePathname();
+  return (
+    <aside>
+      {adminLinks.map((link) => {
+        const isActivePage = pathname === link.href;
+        const variant = isActivePage ? 'secondary' : 'ghost';
+        return (
+          <Button
+            asChild
+            className='w-full mb-2 capitalize font-normal'
+            variant={variant}
+          >
+            <Link key={link.href} href={link.href}>
+              {link.label}
+            </Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
+}
+export default Sidebar;
