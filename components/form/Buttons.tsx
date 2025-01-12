bc897c40fde6980aee051ddcec3c164565137be4
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-12 08:35:51 +0100

    feat: implement SubmitButton component with loading state and customizable size

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,4 +1,45 @@
-function Buttons() {
-  return <div>Buttons</div>;
+'use client';
+
+import { RxReload } from 'react-icons/rx';
+import { useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { LuTrash2 } from 'react-icons/lu';
+import { TbEdit } from 'react-icons/tb';
+
+type btnSize = 'default' | 'lg' | 'sm';
+
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+/** SUBMIT BUTTON */
+export function SubmitButton({
+  className = '',
+  text = 'submit',
+  size = 'lg',
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type='submit'
+      disabled={pending}
+      className={cn('capitalize', className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <RxReload className='mr-2 h-4 w-4 animate-spin' />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
 }
-export default Buttons;
