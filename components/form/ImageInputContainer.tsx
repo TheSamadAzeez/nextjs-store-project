f9391fbece14ba30b1e6aa857c631f65d0df2581
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-23 15:32:07 +0100

    feat: add ImageInputContainer component with props for image handling

--- components/form/ImageInputContainer.tsx
+++ components/form/ImageInputContainer.tsx
@@ -1,3 +1,20 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import FormContainer from './FormContainer';
+import ImageInput from './ImageInput';
+import { SubmitButton } from './Buttons';
+import { type actionFunction } from '@/utils/types';
+
+type ImageInputContainerProps = {
+  image: string;
+  name: string;
+  action: actionFunction;
+  text: string;
+  children?: React.ReactNode;
+};
+
 function ImageInputContainer() {
   return <div>ImageInputContainer</div>;
 }
