846c00b9994585d0fd574e0b883bdac7b51a5980
Samad-Azeez <azeezsamad2004@gmail.com>
2025-01-26 23:18:56 +0100

    feat: implement FavoritesPage to display user's favorite products and add loading state

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,22 @@
-function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+import { fetchUserFavorites } from '@/utils/actions';
+import SectionTitle from '@/components/global/SectionTitle';
+import ProductsGrid from '@/components/products/ProductsGrid';
+
+// Fetches the user's favorite products and displays them in a grid
+async function FavoritesPage() {
+  const favorites = await fetchUserFavorites();
+
+  if (favorites.length === 0) {
+    return <SectionTitle text='You have no favorites yet.' />;
+  }
+
+  return (
+    <div>
+      <SectionTitle text='Favorites' />
+      {/* pass the favorite product to the ProductsGrid component to display the favorite products */}
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
+  );
 }
 
 export default FavoritesPage;
