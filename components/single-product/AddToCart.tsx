17cce8cb308eaa753dfecae88d89657156d25bc4
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-08 21:52:06 +0100

    feat: enhance AddToCart component with product amount selection and user authentication

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,11 +1,41 @@
-import { Button } from '../ui/button';
+'use client';
+import { useState } from 'react';
+import SelectProductAmount from './SelectProductAmount';
+import { Mode } from './SelectProductAmount';
+import FormContainer from '../form/FormContainer';
+import { SubmitButton } from '../form/Buttons';
+import { addToCartAction } from '@/utils/actions';
+import { useAuth } from '@clerk/nextjs';
+import { ProductSignInButton } from '../form/Buttons';
 
+// Component to add a product to the cart
 function AddToCart({ productId }: { productId: string }) {
-  console.log(productId);
+  // State to manage the amount of product to add
+  const [amount, setAmount] = useState(1);
+  // Get the user ID from authentication context
+  const { userId } = useAuth();
+
   return (
-    <Button className='capitalize mt-8' size='lg'>
-      add to cart
-    </Button>
+    <div className='mt-4'>
+      {/* Component to select the amount of product */}
+      <SelectProductAmount
+        mode={Mode.SingleProduct}
+        amount={amount}
+        setAmount={setAmount}
+      />
+      {userId ? (
+        // Form to add product to cart if user is authenticated
+        <FormContainer action={addToCartAction}>
+          <input type='hidden' name='productId' value={productId} />
+          <input type='hidden' name='amount' value={amount} />
+          <SubmitButton text='add to cart' size='default' className='mt-8' />
+        </FormContainer>
+      ) : (
+        // Button to prompt user to sign in if not authenticated
+        <ProductSignInButton />
+      )}
+    </div>
   );
 }
+
 export default AddToCart;
