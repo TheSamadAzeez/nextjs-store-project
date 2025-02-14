ca9089f552442c33e8e3778643037fa1ae42381f
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-14 02:10:15 +0100

    refactor: remove unused imports in CartItemsList component

--- components/cart/CartItemsList.tsx
+++ components/cart/CartItemsList.tsx
@@ -1,7 +1,3 @@
-import { Card } from '@/components/ui/card';
-import { FirstColumn, SecondColumn, FourthColumn } from './CartItemColumns';
-import ThirdColumn from './ThirdColumn';
-
 function CartItemsList() {
   return <div>CartItemsList</div>;
 }
