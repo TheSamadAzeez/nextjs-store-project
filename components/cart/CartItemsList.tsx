c6a39d8e6daed5ff7139f785ad744f7989d45966
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-14 02:01:37 +0100

    feat: add imports for Card and column components in CartItemsList

--- components/cart/CartItemsList.tsx
+++ components/cart/CartItemsList.tsx
@@ -1,3 +1,7 @@
+import { Card } from '@/components/ui/card';
+import { FirstColumn, SecondColumn, FourthColumn } from './CartItemColumns';
+import ThirdColumn from './ThirdColumn';
+
 function CartItemsList() {
   return <div>CartItemsList</div>;
 }
