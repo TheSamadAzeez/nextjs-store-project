b201ff82ea0d7ee5c51e9695b7365256d96377da
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-15 13:30:22 +0100

    feat: added proper comments

--- components/cart/ThirdColumn.tsx
+++ components/cart/ThirdColumn.tsx
@@ -8,10 +8,14 @@ import { removeCartItemAction, updateCartItemAction } from '@/utils/actions';
 import { useToast } from '@/hooks/use-toast';
 
 function ThirdColumn({ quantity, id }: { quantity: number; id: string }) {
+  // State to manage the amount of product
   const [amount, setAmount] = useState(quantity);
+  // State to manage loading status
   const [isLoading, setIsLoading] = useState(false);
+  // Toast hook for displaying messages
   const { toast } = useToast();
 
+  // Function to handle amount change
   const handleAmountChange = async (value: number) => {
     setIsLoading(true);
     toast({ description: 'Updating cart item...' });
@@ -26,12 +30,14 @@ function ThirdColumn({ quantity, id }: { quantity: number; id: string }) {
 
   return (
     <div className='md:ml-8'>
+      {/* Component to select product amount */}
       <SelectProductAmount
         amount={amount}
         setAmount={handleAmountChange}
         mode={Mode.CartItem}
         isLoading={isLoading}
       />
+      {/* Form to remove cart item */}
       <FormContainer action={removeCartItemAction}>
         <input type='hidden' name='id' value={id} />
         <SubmitButton size='sm' className='mt-4' text='remove' />
