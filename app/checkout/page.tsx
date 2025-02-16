982a58d200689420eca725b7c03b171fd0818976
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-16 17:34:30 +0100

    feat: integrate Stripe and Axios for enhanced checkout functionality

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,17 @@
+'use client';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import React, { useCallback } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
 function CheckoutPage() {
   return <div>CheckoutPage</div>;
 }
