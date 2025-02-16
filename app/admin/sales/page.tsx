e29e7a759e3e208b8c8065a26f054b4e23dc33b2
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-16 09:23:32 +0100

    feat: implement SalesPage component to display admin orders in a table

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -1,4 +1,62 @@
-function SalesPage() {
-  return <div>SalesPage</div>;
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+import { fetchAdminOrders } from '@/utils/actions';
+import { formatCurrency, formatDate } from '@/utils/format';
+async function SalesPage() {
+  // Fetch orders data
+  const orders = await fetchAdminOrders();
+
+  return (
+    <div>
+      <Table>
+        {/* Display total number of orders */}
+        <TableCaption>Total orders : {orders.length}</TableCaption>
+        <TableHeader>
+          <TableRow>
+            {/* Define table headers */}
+            <TableHead>Email</TableHead>
+            <TableHead>Products</TableHead>
+            <TableHead>Order Total</TableHead>
+            <TableHead>Tax</TableHead>
+            <TableHead>Shipping</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {/* Map through orders and display each order's details */}
+          {orders.map((order) => {
+            const {
+              id,
+              products,
+              orderTotal,
+              tax,
+              shipping,
+              createdAt,
+              email,
+            } = order;
+
+            return (
+              <TableRow key={id}>
+                <TableCell>{email}</TableCell>
+                <TableCell>{products}</TableCell>
+                <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                <TableCell>{formatCurrency(tax)}</TableCell>
+                <TableCell>{formatCurrency(shipping)}</TableCell>
+                <TableCell>{formatDate(createdAt)}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 export default SalesPage;
