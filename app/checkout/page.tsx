a59a88f6e6aaac86001b23116945b85b0000f4d9
Samad-Azeez <azeezsamad2004@gmail.com>
2025-02-16 21:28:28 +0100

    feat: enhance CheckoutPage with search parameter handling and client secret fetching

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'; // Indicates that this file is a client-side component
 import axios from 'axios';
 import { useSearchParams } from 'next/navigation';
 import React, { useCallback } from 'react';
@@ -8,11 +8,36 @@ import {
   EmbeddedCheckout,
 } from '@stripe/react-stripe-js';
 
+// Load Stripe with the publishable key from environment variables
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 );
 
 function CheckoutPage() {
-  return <div>CheckoutPage</div>;
+  // Get search parameters from the URL
+  const searchParams = useSearchParams();
+
+  // Extract orderId and cartId from search parameters
+  const orderId = searchParams.get('orderId');
+  const cartId = searchParams.get('cartId');
+
+  // Function to fetch the client secret from the backend
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post('/api/checkout', { orderId, cartId });
+    return response.data.clientSecret;
+  }, []);
+
+  // Options for the EmbeddedCheckoutProvider
+  const options = { fetchClientSecret };
+
+  return (
+    <div id='checkout'>
+      {/* Provide the EmbeddedCheckout component with Stripe and options */}
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
 }
+
 export default CheckoutPage;
